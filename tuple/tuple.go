5ad4c0e6390fe314bc818013280d8fb8e6600624
Raúl Pleitez <racsoraul@gmail.com>
2021-10-25 17:52:46 -0600

    Improve tests and adds Tuple Add operation.

--- tuple/tuple.go
+++ tuple/tuple.go
@@ -1,10 +1,13 @@
 package tuple
 
 import (
+	"errors"
 	"fmt"
 	"math"
 	"rt"
 )
+
+var ErrorInvalidAddOp = errors.New("invalid Add operation. Point + Point is not allowed")
 
 // Tuple underlying structure for vector and points.
 type Tuple struct {
@@ -17,13 +20,24 @@ func NewTuple(x, y, z, w float64) Tuple {
 }
 
 func (t Tuple) String() string {
-	return fmt.Sprintf("X: %g, Y: %g, Z: %g, W: %g", t.X, t.Y, t.Z, t.W)
+	return fmt.Sprintf("[X: %g, Y: %g, Z: %g, W: %g]", t.X, t.Y, t.Z, t.W)
+}
+
+func equalFloat(a, b float64) bool {
+	return math.Abs(a-b) < rt.EPSILON
 }
 
 func (t Tuple) IsEqual(to Tuple) bool {
 	return equalFloat(t.X, to.X) && equalFloat(t.Y, to.Y) && equalFloat(t.Z, to.Z) && equalFloat(t.W, to.W)
 }
 
-func equalFloat(a, b float64) bool {
-	return math.Abs(a-b) < rt.EPSILON
+// Add Performs addition of two tuples. Valid operations are
+// Vector + Point (Point + Vector), Vector + Vector. When attempting
+// to add Point + Point r will be the zero-value of Tuple and
+// err will be ErrorInvalidAddOp.
+func Add(a, b Tuple) (r Tuple, err error) {
+	if a.IsPoint() && b.IsPoint() {
+		return Tuple{}, ErrorInvalidAddOp
+	}
+	return NewTuple(a.X+b.X, a.Y+b.Y, a.Z+b.Z, a.W+b.W), nil
 }
--- tuple/tuple_test.go
+++ tuple/tuple_test.go
@@ -4,16 +4,16 @@ import "testing"
 
 func TestIsPoint(t *testing.T) {
 	p := NewPoint(4.3, -4.2, 3.1)
-	if !(p.X == 4.3) {
+	if !equalFloat(p.X, 4.3) {
 		t.Errorf("tuple 'X' value is %v. Expected value was 4.3\n", p.X)
 	}
-	if !(p.Y == -4.2) {
+	if !equalFloat(p.Y, -4.2) {
 		t.Errorf("tuple 'Y' value is %v. Expected value was -4.2\n", p.Y)
 	}
-	if !(p.Z == 3.1) {
+	if !equalFloat(p.Z, 3.1) {
 		t.Errorf("tuple 'Z' value is %v. Expected value was 3.1\n", p.Z)
 	}
-	if !(p.W == 1.0) {
+	if !equalFloat(p.W, 1.0) {
 		t.Errorf("tuple 'W' value is %v. Expected value was 1\n", p.W)
 	}
 	if !p.IsPoint() {
@@ -23,19 +23,62 @@ func TestIsPoint(t *testing.T) {
 
 func TestIsVector(t *testing.T) {
 	v := NewVector(4.3, -4.2, 3.1)
-	if !(v.X == 4.3) {
+	if !equalFloat(v.X, 4.3) {
 		t.Errorf("tuple 'X' value is %v. Expected value was 4.3\n", v.X)
 	}
-	if !(v.Y == -4.2) {
+	if !equalFloat(v.Y, -4.2) {
 		t.Errorf("tuple 'Y' value is %v. Expected value was -4.2\n", v.Y)
 	}
-	if !(v.Z == 3.1) {
+	if !equalFloat(v.Z, 3.1) {
 		t.Errorf("tuple 'Z' value is %v. Expected value was 3.1\n", v.Z)
 	}
-	if !(v.W == 0.0) {
+	if !equalFloat(v.W, 0.0) {
 		t.Errorf("tuple 'W' value is %v. Expected value was 0\n", v.W)
 	}
 	if !v.IsVector() {
 		t.Fatalf("tuple is a point. It was expected to be a vector: %v", v)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	testCases := []struct {
+		name                     string
+		inputA, inputB, expected Tuple
+		err                      error
+	}{
+		{
+			"Point + Vector = Point",
+			NewPoint(3, -2, 5),
+			NewVector(-2, 3, 1),
+			NewPoint(1, 1, 6),
+			nil,
+		},
+		{
+			"Vector + Vector = Vector",
+			NewVector(3, -2, 5),
+			NewVector(-2, 3, 1),
+			NewVector(1, 1, 6),
+			nil,
+		},
+		{
+			"Point + Point = Error",
+			NewPoint(3, -2, 5),
+			NewPoint(6, 9, 1),
+			NewTuple(0, 0, 0, 0),
+			ErrorInvalidAddOp,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := Add(tc.inputA, tc.inputB)
+			if err != tc.err {
+				t.Fatalf("error adding tuples. Unexpected err value: %v; wanted: %v", err, tc.err)
+			}
+
+			if !result.IsEqual(tc.expected) {
+				t.Errorf("got: %v; want: %v", result, tc.expected)
+			}
+		})
 	}
 }
